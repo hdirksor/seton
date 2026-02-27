bc0175b032491123a4e698aaf5b759e06c428595
Henry Dickson <hdickson@Henrys-MacBook-Pro.local>
2026-02-27 12:57:11 -0500

    remove linter and tidy up

--- commands/commands_test.go
+++ commands/commands_test.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/hdicksonjr/seton/linter"
 	"github.com/hdicksonjr/seton/parser"
 )
 
@@ -20,18 +19,6 @@ func (m *MockParser) Parse(directory string, walker parser.Walker) ([]parser.Par
 		fmt.Printf("should error did Error")
 		return nil, fmt.Errorf("parse error")
 	}
-	return nil, nil
-}
-
-type MockLinter struct {
-	shouldError bool
-}
-
-func (m *MockLinter) Lint(filename string) ([]linter.Warning, error) {
-	if m.shouldError {
-		return nil, fmt.Errorf("parse error")
-	}
-
 	return nil, nil
 }
 
@@ -79,50 +66,3 @@ func TestExtractCmd(t *testing.T) {
 	})
 }
 
-func TestLintCmd(t *testing.T) {
-	t.Run("successful lint", func(t *testing.T) {
-		mockLinter := &MockLinter{}
-		cmd := lintCmd(mockLinter)
-
-		output := &bytes.Buffer{}
-
-		cmd.SetOut(output)
-		cmd.SetArgs([]string{"fileName"})
-
-		err := cmd.Execute()
-
-		if err != nil {
-			t.Errorf("unexpected Error: %q", err)
-		}
-	})
-
-	t.Run("failed lint", func(t *testing.T) {
-		mockLinter := &MockLinter{shouldError: true}
-		cmd := lintCmd(mockLinter)
-
-		output := &bytes.Buffer{}
-		cmd.SetOut(output)
-		cmd.SetArgs([]string{"testdir"})
-
-		stdout := os.Stdout
-		r, w, _ := os.Pipe()
-		os.Stdout = w
-
-		err := cmd.Execute()
-
-		w.Close()
-		os.Stdout = stdout
-
-		var buf bytes.Buffer
-		buf.ReadFrom(r)
-
-		if err != nil {
-			t.Errorf("unexpected Error: %q", err)
-		}
-		if !bytes.Contains(buf.Bytes(), []byte("parse error")) {
-			t.Logf("output buffer: %s", buf.String())
-			t.Errorf("expected error message not found in output: %q", buf.String())
-		}
-	})
-
-}
